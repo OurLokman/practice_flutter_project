c97c61afa8b4d5b2fa13567fe22a411ab6883f92
Dart Basic to Advanced <mhlokman10@gmail.com>
2024-07-17 20:25:35 +0600

    show modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -8,37 +8,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('Modal Bottom Sheet Example')),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              showModalBottomSheet(
-                context: context,
-                builder: (context) {
-                  return Container(
-                    height: 200,
-                    color: Colors.white,
-                    child: Column(
-                      children: [
-                        ListTile(
-                          leading: Icon(Icons.photo),
-                          title: Text('Photo'),
-                          onTap: () {},
-                        ),
-                        ListTile(
-                          leading: Icon(Icons.music_note),
-                          title: Text('Music'),
-                          onTap: () {},
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              );
-            },
-            child: Text('Show Modal Bottom Sheet'),
-          ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return Container(
+                  height: 200,
+                  color: Colors.white,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.photo),
+                        title: Text('Photo'),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.music_note),
+                        title: Text('Music'),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+          child: Text('Show Bottom Sheet'),
         ),
       ),
     );
